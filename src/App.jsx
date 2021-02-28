eb01c6e974bc1305316610c952b95fc38cb06b0c
Mind0Bender <mrcircuit1234@gmail.com>
2021-03-01 00:10:49 +0530

    ajeeb sa Register page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import RegisterPage from "./pages/registerPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey There</h1>
+      <RegisterPage />
     </div>
   );
 }
