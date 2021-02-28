66517fd470b9c12b1a0c9876d30ce5e7743caf6e
kdeuser <krishhtrishh9304@gmail.com>
2021-02-28 14:28:59 +0530

    styling homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Homepage from "./pages/homepage";
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey There</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <Homepage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/logo/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <span className="link">Link</span>
+      <span className="up">Up</span>
+    </div>
+  );
+};
+
+export default Logo;
